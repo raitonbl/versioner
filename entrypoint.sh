0605cc56bb7e5d3a7bd31226f0a24cc66965f0a3
Delfim Marime <Delfimarime@gmail.com>
2022-06-17 21:09:41 +0200

    wip: github-action build

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+curl "https://s3.af-south-1.amazonaws.com/binaries.raitonbl.com/public/versioner/$1/versioner" --output versioner
+chmod a+x versioner
+mv versioner /usr/local/bin/versioner
+
+ARGS="versioner $2 $3 $4 $5"
+
+eval "$ARGS"
