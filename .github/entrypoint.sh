b89663758479f34d7529fc7fb6561f0d7cc9256c
Delfim Marime <Delfimarime@gmail.com>
2022-06-18 19:47:04 +0200

    fix: docker images issues

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-cd "/github/workspace"
-eval "versioner $1 $2 $3 $4 $5"
+
+if [ "$1" == "get" ]; then
+  eval "versioner $1 $2 $3 $4 $5"
+elif [ "$1" == "set" ]; then
+   eval "versioner $1 $2 $3 $4 $5"
+elif [ "$1" == "set-stamped-version" ]; then
+  eval "versioner $1 $2 $3 $4 $5"
+else
+    echo "command $1 not supported"
+    exit 1
+fi
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:latest
 ADD dist/versioner /usr/local/bin/versioner
-RUN export PATH=/usr/local/bin/versioner:${PATH}
 RUN chmod a+x /usr/local/bin/versioner
 ENTRYPOINT ["versioner"]
+CMD ["help"]
